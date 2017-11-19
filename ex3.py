c7e537699c49e4ab3790332193ff8f0875cfbf06
arsenal-ozil <yangchen623@hotmail.com>
2017-11-19 18:21:11 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,24 @@
+#-*- coding:utf-8 -*-
+print "I will now count my chickens" 
+
+print "hens", 25+30/6
+print "Roosters", 100-25*3%4
+
+print "Now I will count my eggs"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2<5-7"
+
+print 3+2<5-7 #判断结果为错误，输出False
+
+print "what is 3+2?", 3+2
+print "what is 5-7?", 5-7
+
+print "oh that's why it's false."
+
+print "how about some more."
+
+print "Is it greater?", 5>-2 #True or False? true
+print "Is it greater or equal?", 5>=-2 #判断为正确 输出true
+print "Is it less or equal?",5<=-2 #判断为错误 输出false
